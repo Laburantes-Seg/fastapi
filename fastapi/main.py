c163e4ac48080d77324da3247bad018691ad0722
Laburantes-Seg <soporte.laburantes@gmail.com>
2025-08-22 20:49:13 -0300

    Add files via upload

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,4 @@
-""" Probado para edición de descripciones
+""" Probado en SQlite el 7 de julio de 2025 y listo para migrar a PostGrade 
 """
 from datetime import datetime, timezone
 from fastapi import FastAPI, HTTPException, Depends, status
@@ -483,4 +483,8 @@ def update_penales(
 
     return db_trabajador
 
-
+####################
+@app.get("/ping")
+def ping():
+    print("🔔 PING recibido")
+    return {"ok": True}
